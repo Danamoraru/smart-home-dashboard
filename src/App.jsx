396ffaf2dff7d23f73a48a8f91241bd108487893
dana <dm0624413@gmail.com>
2026-07-15 22:58:03 +0300

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+
+      <p>Bine ai venit în aplicația mea de licență!</p>
+    </div>
+  );
+}
+
+export default App;
